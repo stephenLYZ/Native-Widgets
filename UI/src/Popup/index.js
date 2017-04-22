8c939e0de6ba245a2b2de7a4ab39e7645108104e
stephenliu <stephenlyao@yeah.net>
2017-04-22 23:23:14 +0800

    popup

---
+++ UI/src/Popup/index.js
@@ -0,0 +1,43 @@
+import './popup.scss'
+
+class Popup {
+  constructor(options) {
+    this.container = options.container
+    this.title = options.title
+    this.content = options.content
+    this.open = options.open
+    this.close = options.close
+    this.isDrag = options.isDrag
+    this.isMask = options.isMask
+    this.width = options.width
+    this.height = options.height
+  }
+
+  init() {
+    let container = document.createElement('div')
+    container.classList.add(this.container)
+    this.renderHeader(container)
+    document.body.appendChild(container)
+  }
+
+  renderHeader(container) {
+    let header = document.createElement('div')
+    header.classList.add('header')
+    header.innerHTML = this.title
+    container.appendChild(header)
+  }
+}
+
+Popup.DEFAULT = {
+  container: 'popup',
+  title: 'Popup UI Component',
+  content: 'This a popup ui component, by stephenliu',
+  open: function() { console.log('open popup!') },
+  close: function() { console.log('close popup!') },
+  isDrag: true,
+  isMask: true,
+  width: 400,
+  height: 200
+}
+
+export default Popup
---
+++ UI/src/index.js
@@ -0,0 +1,32 @@
+import './app.scss'
+import Popup from './Popup'
+
+class App {
+  constructor() {
+  }
+
+  init() {
+    this.renderButton({
+      name: '弹出框',
+      handler: this.renderPopup
+    })
+  }
+
+  renderButton(option) {
+    let button = document.createElement('button')
+    let name = document.createTextNode(option.name)
+    button.classList.add('btn')
+    button.appendChild(name)
+    document.body.appendChild(button)
+    button.addEventListener('click', option.handler, false)
+  }
+
+  renderPopup(e) {
+    e.preventDefault()
+    let popup = new Popup(Popup.DEFAULT)
+    popup.init()
+  }
+}
+
+let app = new App()
+app.init()
